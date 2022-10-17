68d8e590375e3bc205b88cdb95b4d9e3801431fa
hailrakeqq <boklanura@gmail.com>
2022-10-17 12:35:52 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace TelegramTestProject
+{
+
+    class Program
+    {
+        static ITelegramBotClient bot = new TelegramBotClient("5521651618:AAHW4h-7FvTDAevAzMyRbYucp58meyjIyV4");
+        public static async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            // логирование в json 
+            //Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(update));
+
+            if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
+            {
+                var message = update.Message;
+                if (message.Text.ToLower() == "/start")
+                {
+                    SendInline(botClient: botClient, chatId: message.Chat.Id, cancellationToken: cancellationToken);
+                    return;
+                }
+            }
+            if (update.Type == Telegram.Bot.Types.Enums.UpdateType.CallbackQuery)
+            {
+                string codeOfButton = update.CallbackQuery.Data;
+                if (codeOfButton == "test1")
+                {
+                    Console.WriteLine("Нажата Кнопка 1");
+                    string telegramMessage = "Вы нажали Кнопку 1";
+
+                    //await botClient.SendTextMessageAsync(chatId: update.CallbackQuery.Message.Chat.Id, telegramMessage, parseMode: Telegram.Bot.Types.Enums.ParseMode.Html);
+                }
+                if (codeOfButton == "test2")
+                {
+                    Console.WriteLine("Нажата Кнопка 2");
+                    string telegramMessage = "Вы нажали Кнопку 2";
+                    // await botClient.SendTextMessageAsync(chatId: update.CallbackQuery.Message.Chat.Id, telegramMessage, parseMode: Telegram.Bot.Types.Enums.ParseMode.Html);
+
+                    InlineKeyboardMarkup inlineKeyBoard = new InlineKeyboardMarkup(
+                        new[]
+                        {
+                            // first row
+                            new[]
+                            {
+                                // first button in row
+                                InlineKeyboardButton.WithCallbackData(text: "1", callbackData: "test1"),
+                                // second button in row
+                                InlineKeyboardButton.WithCallbackData(text: "2", callbackData: "test2"),
+                            },
+
+                        });
+
+                    // // await botClient.EditMessageCaptionAsync(chatId: update.CallbackQuery.Message.Chat.Id, caption: telegramMessage, messageId: update.CallbackQuery.Message.MessageId);
+                    // await bot.EditMessageTextAsync(update.CallbackQuery.Message.Chat.Id, update.CallbackQuery.Message.MessageId, telegramMessage, replyMarkup: inlineKeyBoard, parseMode: Telegram.Bot.Types.Enums.ParseMode.Html);
+                }
+            }
+        }
+
+        public static async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            // Некоторые действия
+            Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(exception));
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Start listening for @{bot.GetMeAsync().Result.Username}");
+
+           
+            var cts = new CancellationTokenSource();
+            var cancellationToken = cts.Token;
+            var receiverOptions = new ReceiverOptions
+            {
+                AllowedUpdates = { }, 
+            };
+            bot.StartReceiving(
+                HandleUpdateAsync,
+                HandleErrorAsync,
+                receiverOptions,
+                cancellationToken
+            );
+            Console.ReadLine();
+        }
+
+        public static async void SendInline(ITelegramBotClient botClient, long chatId, CancellationToken cancellationToken)
+        {
+            InlineKeyboardMarkup inlineKeyboard = new InlineKeyboardMarkup(
+                // keyboard
+                new[]
+                {
+                    // first row
+                    new[]
+                    {
+                        // first button in row
+                        InlineKeyboardButton.WithCallbackData(text: "1", callbackData: "test1"),
+                        // second button in row
+                        InlineKeyboardButton.WithCallbackData(text: "2", callbackData: "test2"),
+                    },
+                });
+            Message sentMessage = await botClient.SendTextMessageAsync(
+                chatId: chatId,
+                text: "test",
+                replyMarkup: inlineKeyboard,
+                cancellationToken: cancellationToken);
+        }
+    }
+}
